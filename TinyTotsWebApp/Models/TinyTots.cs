0a3cc8d649e0f4dba8f82e4df062f9bb1fb4a405
Seng Yik <sengyik77@gmail.com>
2024-11-16 22:27:12 +0800

    Add project files.

---
+++ TinyTotsWebApp/Models/TinyTots.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TinyTotsWebApp.Models
+{
+    public class TinyTots
+    {
+        [Key]
+        public int RegistrationId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string SecondName { get; set; }
+        [Required]
+        public string Race { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        public string FoodAllergires { get; set; }
+    }
+}
---
+++ TinyTotsWebApp/Models/TinyTotsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TinyTotsWebApp.Models
+{
+    public class TinyTotsDbContext : DbContext
+    {
+        public DbSet<TinyTots> TinyTotsTable { get; set; }
+    }
+    // <TinyTots> refers to TinyTotsmodel 
+    // TinyTotsTable refers to the table name that will store the data in the database
+}
